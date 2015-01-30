6e5dd9767064274a9f21e98fa0688f4c3db9b6bb
phidaika94 <thesonofdeath_94@yahoo.com.vn>
2015-01-30 18:44:34 +0700

    edit little text in evaluation

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mysql=require('mysql');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var logout=require('./routes/logout');
+var evaluation=require('./routes/evaluation');
 var app = express();
 
 // view engine setup
@@ -27,6 +28,7 @@ app.use(session({secret:'deo ai biet dau',resave:false, saveUninitialized: true}
 app.use('/', routes);
 app.use('/users', users);
 app.use('/logout',logout);
+app.use('/evaluation',evaluation);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
