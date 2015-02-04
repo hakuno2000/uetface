61d3cbca5859395fa1ea1be591547088da3dfb9e
phidaika94 <thesonofdeath_94@yahoo.com.vn>
2015-02-04 18:53:39 +0700

    update evaluation

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -3,11 +3,74 @@
  */
 var express=require('express');
 var router=express.Router();
-
+var mysql=require('mysql');
 router.get('/',function(req,res,next){
-    res.render('evaluation');
+    if(req.session.username){
+        res.render('evaluation');
+    }
+    else{
+        res.redirect('/');
+    }
 });
 router.post('/',function(req,res,next){
-    res.render('evaluation');
+    if(req.session.username){
+        console.log(req.session.user_id);
+        if(req.body.subject!=''&&req.body.sub_id!=''&&req.body.cla_id!==''&&
+            req.body.tea_id!=''&&req.body.cla_name!=''&&req.body.tea_name!=''&&req.body.date!=''
+            &&req.body.q1!=''&&req.body.q2!=''&&req.body.q3!=''&&req.body.q4!=''&&req.body.q5!=''&&req.body.q6!=''
+            &&req.body.q7!=''&&req.body.q8!=''&&req.body.q9!=''&&req.body.q10!=''&&req.body.q11!=''&&req.body.q12!=''
+            &&req.body.q13!=''&&req.body.q14!=''&&req.body.q15!=''&&req.body.q16!=''&&req.body.q17!=''
+            &&req.body.q18!=''&&req.body.q19!=''&&req.body.q20!=''&&req.body.q21!=''&&req.body.q22!=''&&req.body.q23!=''
+        ){
+            var connectConfig={
+                host:'localhost',
+                user:'root',
+                password:'root',
+                database:'uetface'
+            };
+            var connection=mysql.createConnection(connectConfig);
+            var form={
+                ma_mon:req.body.sub_id,
+                ma_giang_vien:req.body.tea_id,
+                ma_sinh_vien:req.session.user_id,
+                1:req.body.q1,
+                2:req.body.q2,
+                3:req.body.q3,
+                4:req.body.q4,
+                5:req.body.q5,
+                6:req.body.q6,
+                7:req.body.q7,
+                8:req.body.q8,
+                9:req.body.q9,
+                10:req.body.q10,
+                11:req.body.q11,
+                12:req.body.q12,
+                13:req.body.q13,
+                14:req.body.q14,
+                15:req.body.q15,
+                16:req.body.q16,
+                17:req.body.q17,
+                18:req.body.q18,
+                19:req.body.q19,
+                20:req.body.q20,
+                21:req.body.q21,
+                22:req.body.q22,
+                23:req.body.q23,
+                comment:req.body.comment,
+                date:req.body.date
+            };
+            console.log(form);
+            connection.query('INSERT INTO ketquadanhgia SET ?',form,function(err,results){
+                if(err) throw err;
+                else{
+                    res.render('evaluation',{Rp_Form:'Đánh giá thành công.'})
+                }
+            });
+        }
+    }
+    else{
+        res.redirect('/');
+    }
+
 });
 module.exports=router;
