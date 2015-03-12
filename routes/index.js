ec015559657b3084037c49aa3de254c19e514ebc
phiph_57 <phiph_57@bitbucket.org>
2015-03-13 03:10:13 +0700

    update register module

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var mongoose=require('mongoose');
 var dbURL=require('./data/dbURL');
 var md5=require('./decode/md5');
 var secretKey=require('./secretKey');
+var isNull=require('./isNull');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(req.session.username){
         res.redirect('/users');
     }else {
-        res.render('index', { title: 'UETFace' });
+        res.render('index', { title: 'UETFACE' });
     }
 });
 router.post('/',function(req,res,next){
@@ -18,7 +19,6 @@ router.post('/',function(req,res,next){
         if(mongoose.connection.readyState==0) mongoose.connect(dbURL);
         user_login.findOne({'tai_khoan':req.body.username},function(err,result){
             if (err) console.log(err);
-            var isNull=require('./isNull');
             if(!isNull(result)){
                 if (md5.MD5(req.body.password+secretKey).toString() == result.mat_khau) {
                     req.session.username = req.body.username;
@@ -32,30 +32,73 @@ router.post('/',function(req,res,next){
             if(mongoose.connection.readyState==1) mongoose.disconnect();
         });
     }else if(req.body.login){
-        res.render('index',{title:'UETFace',Log_rp:'Chưa nhập tài khoản hoặc mật khẩu!'});
+        res.render('index',{title:'UETFACE',Log_rp:'Chưa nhập tài khoản hoặc mật khẩu!'});
     }else if(req.body.reg_user!=''&&req.body.reg_email!=''&&req.body.reg_pass!=''&&req.body.std_name!=''
-            &&req.body.std_grade!=''&&req.body.std_class!=''&&req.body.std_id!=''){
+            &&req.body.std_class!=''&&req.body.std_id!=''){
         var user_reg=require('./data/models/user_reg');
-        usr_reg.findOne({'ma_sinh_vien':req.body.std_id},function(err,result){
-            if(!isNull(result)){ res.render('index',{})};
-        });
+        if(mongoose.connection.readyState==0) mongoose.connect(dbURL);
+        if(req.body.reg_user.length<6){
+            res.render('index',{title:'UETFACE',Reg_rp:'Tài khoản nhỏ hơn 6 kí tự.'});
+            if(mongoose.connection.readyState==1) mongoose.disconnect();
+        }else{
+            if(req.body.reg_pass.length<6){
+                res.render('index',{title:'UETFACE',Reg_rp:'Mật khẩu nhỏ hơn 6 kí tự.'});
+                if(mongoose.connection.readyState==1) mongoose.disconnect();
+            }else{
+                user_reg.findOne({'ma_sinh_vien':req.body.std_id},function(err,result){
+                    if(err) {
+                        res.render('index',{title:'UETFACE',Reg_rp:'Đã có lỗi xảy ra. Mời bạn thao tác lại'});
+                        if(mongoose.connection.readyState==1) mongoose.disconnect();
+                    }
+                    if(isNull(result)){ res.render('index',{title:'UETFACE',Reg_rp:'Mã sinh viên không tồn tại'});}
+                    else{
+                        if(result.active==0){
+                            user_reg.findOne({'tai_khoan':req.body.reg_user.toLowerCase()},function(err,result){
+                                if(err) {
+                                    res.render('index',{title:'UETFACE',Reg_rp:'Đã có lỗi xảy ra. Mời bạn thao tác lại'});
+                                    if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                }
+                                if(!isNull(result)){
+                                    res.render('index',{title:'UETFACE',Reg_rp:'Tên tài khoản đã được đăng kí. Mời bạn chọn tài khoản khác.'});
+                                    if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                }
+                                else{
+                                    user_reg.findOne({'email':req.body.reg_email.toLowerCase()},function(err,result){
+                                        if(err) {
+                                            res.render('index',{title:'UETFACE',Reg_rp:'Đã có lỗi xảy ra. Mời bạn thao tác lại'});
+                                            if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                        }
+                                        if(!isNull(result)){
+                                            res.render('index',{title:'UETFACE',Reg_rp:'Email đã được đăng kí. Mời bạn chọn email khác.'});
+                                            if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                        }else{
+                                            user_reg.update({'ma_sinh_vien':req.body.std_id},{$set:{'tai_khoan':req.body.reg_user,
+                                                'mat_khau':md5.MD5(req.body.reg_pass.toLowerCase()+secretKey).toString(),'email':req.body.reg_email.toLowerCase(),
+                                                'active':1,'gioi_tinh':req.body.sex.toLowerCase()}},function(err,result){
+                                                if(err) {
+                                                    console.log(err);
+                                                    res.render('index',{title:'UETFACE',Reg_rp:'Đã có lỗi 1 xảy ra. Mời bạn thao tác lại'});
+                                                    if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                                }
+                                                res.render('index',{title:'UETFACE',Reg_rp:'Chúc mừng bạn đã đăng kí tài khoản thành công!'});
+                                                if(mongoose.connection.readyState==1) mongoose.disconnect();
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }else{
+                            res.render('index',{title:'UETFACE',Reg_rp:'Tài khoản đã được đăng kí!'});
+                            if(mongoose.connection.readyState==1) mongoose.disconnect();
+                        }
+                    }
+                });
+            }
+        }
     }
     else{
         res.render('index',{title:'Uet Face',Reg_rp:'Bạn đã nhập thiếu thông tin.'});
     }
 
-
-})
-function Register(req,res,User,connection){
-    connection.query('INSERT INTO sinhvien SET ?',User,function(err,results){
-        if(err) throw err;
-    });
-    connection.end(function(err){
-        req.session.username=User.tai_khoan;
-        req.session.password=User.mat_khau;
-        req.session.user_id=User.ma_sinh_vien;
-        res.render('index',{title:'UETFace',Reg_rp:'Đăng kí thành công.'});
-        if(err) throw err;
-    });
-}
+});
 module.exports = router;
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 ﻿
-var mongoose=require('mongoose');
+//var mongoose=require('mongoose');
 /*
 function doit(next){
     var user_reg=require('./routes/data/models/user_reg');
@@ -12,32 +12,32 @@ function doit(next){
 }
 */
 
-function doContinue(){
-
-    var user_reg=require('./routes/data/models/user_reg');
-    if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
-    user_reg.findOne({'ma_sinh_vien':'12020282'},function(err,result){
-        console.log(result.ma_sinh_vien);
-        if(mongoose.connection.readyState==1)mongoose.disconnect();
-    });
-}
+//function doContinue(){
+//
+//    var user_reg=require('./routes/data/models/user_reg');
+//    if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
+//    user_reg.findOne({'ma_sinh_vien':'12020282'},function(err,result){
+//        console.log(result.ma_sinh_vien);
+//        if(mongoose.connection.readyState==1)mongoose.disconnect();
+//    });
+//}
 
 //doit(doContinue);
-var user_reg=require('./routes/data/models/user_reg');
-if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
-user_reg.find({'ma_sinh_vien':/8/i},function(err,result){
-    console.log(result);
-    user_reg.findOne({'ma_sinh_vien':'12020282'},function(err,result){
-        console.log(result.ma_sinh_vien);
-        if(mongoose.connection.readyState==1)mongoose.disconnect();
-    });
 
-});
+//var user_reg=require('./routes/data/models/user_reg');
+//if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
+//user_reg.find({'ma_sinh_vien':/8/i},function(err,result){
+//    console.log(result);
+//    user_reg.findOne({'ma_sinh_vien':'12020282'},function(err,result){
+//        console.log(result.active);
+//        if(mongoose.connection.readyState==1)mongoose.disconnect();
+//    });
+//
+//});
 
 
 
-/*
+
 var md5=require('./routes/decode/md5');
-var test=md5.MD5('levannguyen'+'uetface').toString();
+var test=md5.MD5('khoa'+'uetface').toString();
 console.log(test);
-*/
