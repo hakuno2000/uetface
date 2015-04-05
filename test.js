0f182ccd1aaafdfef9575719a615eb7d458956ac
phiph_57 <phiph_57@bitbucket.org>
2015-04-06 02:03:57 +0700

    update timetable full

--- test.js
+++ test.js
@@ -44,42 +44,33 @@ function doit(next){
 
 var mongoose = require('mongoose')
     , Schema = mongoose.Schema;
-if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
-var deepPopulate = require('mongoose-deep-populate');
-var theory_Schema=new mongoose.Schema({
-    ma_sinh_vien:String,
-    ma_lop:String,
-    ghi_chu:String,
-    thong_tin_lop : { type: Schema.Types.ObjectId, ref: 'lopmonhoc' }
-});
-theory_Schema.plugin(deepPopulate);
-var theory=mongoose.model('sinhvien_lopmonhoc',theory_Schema,'sinhvien_lopmonhoc');
-var theory_info_schema=new mongoose.Schema({
-    ma_lop:String,
-    ma_mon:String,
-    ghi_chu:String,
-    tiet_bat_dau:Number,
-    tiet_ket_thuc:Number,
-    thu: String,
-    giang_duong: String,
-    si_so:Number,
-    ma_danh_gia:String,
-    thong_tin_mon: { type: Schema.Types.ObjectId, ref: 'monhoc' }
-});
-var subject_schema=new mongoose.Schema({
-    ma_mon:String,
-    ten_mon: String,
-    khoa: String,
-    ma_danh_gia:String,
-    tin_chi:Number,
-    mo_ta:String
-});
-var subject=mongoose.model('monhoc',subject_schema,'monhoc');
-theory_info_schema.plugin(deepPopulate);
-var theory_info=mongoose.model('lopmonhoc',theory_info_schema,'lopmonhoc');
-theory.find({ma_sinh_vien:'12020282'},{_id:0}).deepPopulate('thong_tin_lop.thong_tin_mon').exec(function(err,_result){
-    console.log(_result);
-});
+//if(mongoose.connection.readyState==0) mongoose.connect('mongodb://localhost/uetface');
+var practice=require('./routes/data/models/practice');
+var theory=require('./routes/data/models/theory');
+var theory_info=require('./routes/data/models/theory_info');
+var isNull=require('./routes/isNull');
+practice.find().exec(function(err,result){
+    result.forEach(function(value){
+        theory.find({ma_lop: value.ma_lop,ma_sinh_vien: value.ma_sinh_vien}).exec(function(err,result2){
+            theory_info.find({ma_lop:value.ma_lop}).exec(function(err,result3){
+                theory.update({ma_lop: value.ma_lop,ma_sinh_vien: value.ma_sinh_vien},{$set:{thong_tin_lop: result3[0]._id}},function(err,result4){
+                    console.log(result4);
+                })
+            })
+                //console.log(value.thong_tin_lop);
+
+                //var add=new theory({
+                //    ma_sinh_vien:value.ma_sinh_vien,
+                //    ma_lop: value.ma_lop,
+                //    ghi_chu: 'CL',
+                //    thong_tin_lop: value.thong_tin_lop
+                //});
+                //add.save(function(err){
+                //    if(err) console.log(err);
+                //})
+        });
+    });
+})
 //theory_info.find().deepPopulate('thong_tin_mon').exec(function(err,result){
 //    console.log(result);
 //})
