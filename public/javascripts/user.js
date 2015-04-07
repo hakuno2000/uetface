deea5d98233bfa5ed730e6a04a4d6ea9c5fbad99
unknown <nguyenlv_57@vnu.edu.vn>
2015-04-08 00:47:06 +0700

    update Thoi khoa bieu user

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,13 +1,57 @@
 /**
  * Created by Phi on 3/30/2015.
  */
+var day, i , number;
+function xoa(tiet ,so_tiet){
+    var part = tiet.split("_");
+    var so = parseInt(part[1]);
+
+    for (var i = 1; i < so_tiet; i++){
+        var phantu = document.getElementById(part[0] + "_" + (so + i));
+        phantu.parentNode.removeChild(phantu);
+    }
+    document.getElementById(tiet).setAttribute("rowspan",so_tiet);
+}
+function chuyen(thu){
+    if(thu == 2) return "mon";
+    if(thu == 3) return "tue";
+    if(thu == 4) return "wed";
+    if(thu == 5) return "thu";
+    if(thu == 6) return "fri";
+    if(thu == 7) return "sat";
+    if(thu == "Ch? nh?t") return "sun";
+}
 var user=angular.module('user',[]);
 user.controller('timetable',function($scope,$http,$q){
     $http.get('/api/user/find_class')
         .success(function(data){
             $scope.theories=data[0];
             $scope.practices=data[1];
+
+            //alert($scope.theories[0].thong_tin_lop.thong_tin_mon.ten_mon);
+
+            for(i = 0 ; i < $scope.theories.length ; i++){
+                day = chuyen($scope.theories[i].thong_tin_lop.thu )+ "_" + $scope.theories[i].thong_tin_lop.tiet_bat_dau;
+                number = $scope.theories[i].thong_tin_lop.tiet_ket_thuc - $scope.theories[i].thong_tin_lop.tiet_bat_dau + 1 ;
+                xoa(day,number);
+
+                $("#"+day).html($scope.theories[i].thong_tin_lop.thong_tin_mon.ten_mon +"<br>(" + $scope.theories[i].thong_tin_lop.giang_duong + ")");
+            }
+
+
+            for(i = 0 ; i < $scope.practices.length ; i++){
+                day = chuyen($scope.practices[i].thong_tin_lop.thu )+ "_" + $scope.practices[i].thong_tin_lop.tiet_bat_dau;
+                number = $scope.practices[i].thong_tin_lop.tiet_ket_thuc - $scope.practices[i].thong_tin_lop.tiet_bat_dau + 1 ;
+                xoa(day,number);
+
+                $("#"+day).html($scope.practices[i].thong_tin_lop.thong_tin_mon.ten_mon +"<br>(" + $scope.practices[i].thong_tin_lop.giang_duong + ")");
+            }
+
         }).error(function(data){
             console.log(data);
         });
 });
+
+
+
+
