63561c8dd09421b0495a02baaf3e959c449a551f
unknown <nguyenlv_57@vnu.edu.vn>
2015-04-16 00:58:37 +0700

    update1

--- routes/api/findTeacherById.js
+++ routes/api/findTeacherById.js
@@ -16,17 +16,35 @@ router.get('/:tea_id',function(req,res){
             res.json({type:'error',content:'Lỗi server!'});
         }
         if(isNull(result2)){
-            //teacher.find({ho_va_ten:new RegExp(rq_id,'i')},{_id:0,ma_giang_vien:1,ho_va_ten:1},function(err,result3){
-            //    if(err) {
-            //        res.json({type:'error',content:'Lỗi server!'});
-            //    }
-            //    if(isNull(result3)){
+            teacher.find({ho_va_ten:new RegExp(rq_id,'i')},{_id:0,ma_giang_vien:1,ho_va_ten:1},function(err,result3){
+                if(err) {
+                    res.json({type:'error',content:'Lỗi server!'});
+                }
+                if(isNull(result3)){
                     res.json({type:'error',content:'Không có thông tin liên quan'});
-            //    }
-            //    else{
-            //        res.json(result3);
-            //    }
-            //});
+                }
+                else{
+                    res.json(result3);
+                }
+            });
+        }
+        else{
+            res.json(result2);
+        }
+    });
+
+});
+router.get('/',function(req,res){
+
+    var teacher=require('./../data/models/teacher');
+    if(mongoose.connection.readyState==0) mongoose.connect(dbURL);
+
+    teacher.find({},{_id:0,ma_giang_vien:1,ho_va_ten:1}).exec(function(err,result2){
+        if(err) {
+            res.json({type:'error',content:'Lỗi server!'});
+        }
+        if(isNull(result2)){
+            res.json({type:'error',content:'Không có thông tin liên quan'});
         }
         else{
             res.json(result2);
