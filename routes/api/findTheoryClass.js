c81751970c51bc183bc333e8fe0f5b3bed07f309
unknown <nguyenlv_57@vnu.edu.vn>
2015-04-17 10:06:30 +0700

    a

--- routes/api/findTheoryClass.js
+++ routes/api/findTheoryClass.js
@@ -12,29 +12,6 @@ router.get('/',function(req,res){
     var theory_info=require('./../data/models/theory_info');
     var subject=require('./../data/models/subjects');
     if(mongoose.connection.readyState==0) mongoose.connect(dbURL);
-    //async.waterfall([
-    //    function(next){
-    //        theory.find({'ma_sinh_vien':req.session.user_id},{_id:0}).exec(next);
-    //    },
-    //    function(result,next){
-    //        var number=result.length;
-    //        function callback(value){
-    //            result.tiet_bat_dau=value.tiet_bat_dau;
-    //        }
-    //        for(var i=0;i<result.length;i++){
-    //            theory_info.find({ma_lop:result[i].ma_lop},{_id:0},function(err,result3){
-    //
-    //                if(i>=result.length-1){
-    //                    res.json(result);
-    //                    console.log(result);
-    //                }
-    //            })
-    //        }
-    //    }
-    //],function(err,result){
-    //    if(err) console.log(err);
-    //    res.json(result);
-    //})
     theory.find({'ma_sinh_vien':req.session.user_id},{_id:0}).deepPopulate('thong_tin_lop.thong_tin_mon').exec(function(err,result){
         if(err) {
             res.json({type:'error',content:'Lỗi server!'});
--- routes/api/user/user.js
+++ routes/api/user/user.js
@@ -7,7 +7,9 @@ var user_new=require('./user_new');
 var find_dgmh=require('./find_dgmh');
 var find_class=require('./findClasses');
 var create_dgmh=require('./create_dgmh');
+var add_class=require('./add_class');
 
+router.use('/add_class',add_class);
 router.use('/new',user_new);
 router.use('/find_evaluate',find_dgmh);
 router.use('/find_class',find_class);
